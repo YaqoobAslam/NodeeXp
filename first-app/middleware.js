22f65ab9e83d334c3d286fd8afe572fd44e9e521
Yaqoob <happyyaqoob@gmail.com>
2021-02-21 11:32:09 -0800

    Created Middleware to check Current URL

---
+++ first-app/middleware.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+const checkUrl = function(req,res,next) {
+  console.warn('Current URL:' + req.originalUrl);
+  next()
+}
+app.use(checkUrl)
+
+
+app.get('/',function(req, res){
+  res.send('This is Home Page')
+})
+app.get('/about', function(req,res){
+  res.send('This is About Page')
+})
+app.get('/login', function(req,res){
+  res.send('This is Login Page')
+})
+app.listen(4000)
