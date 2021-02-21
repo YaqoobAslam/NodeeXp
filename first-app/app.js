5044b569bd9f0baa7721b1de53e35fd245ac37f7
Yaqoob <happyyaqoob@gmail.com>
2021-02-21 02:16:21 -0800

    Created Anonymous function and passes as callback

--- first-app/app.js
+++ first-app/app.js
@@ -1,5 +1,20 @@
-var http = require('http')
-http.createServer(function(req, res){
-  res.write('Hello from Node Server')
-  res.end()
-}).listen(5000)
+// var http = require('http')
+// http.createServer(function(req, res){
+//   res.write('Hello from Node Server')
+//   res.end()
+// }).listen(5000)
+
+
+// ------------------------------------------------
+
+//Created Anonymous function
+var add = function(a,b){
+  return a + b;
+}
+
+function callBack(add){
+  console.warn(300,300)
+}
+
+// Passes add function as callback
+callBack(add)
