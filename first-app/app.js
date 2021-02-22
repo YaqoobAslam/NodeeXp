e2cf3d2e7c68a46f2773f99b0bf2b308a3b103ac
Yaqoob <happyyaqoob@gmail.com>
2021-02-22 00:42:27 -0800

    Template Engine (ejs) display name

--- first-app/app.js
+++ first-app/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const app = express()
-app.set('view engine', 'ejs')
-app.get("/", function (req, res) {
-
-  res.render('Profile')
+const app = express();
+app.set("view engine", "ejs");
+app.get("/profile/:name", function (req, res) {
+  console.warn(req.params.name);
+  res.render("Profile", { name: req.params.name });
 });
 
 app.listen(4000);
