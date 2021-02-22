c6827556c4e67f457577e55d490e89b3a2110d8c
Yaqoob <happyyaqoob@gmail.com>
2021-02-21 23:23:40 -0800

    Open file with Router display different pages

--- first-app/middleware.js
+++ first-app/middleware.js
@@ -8,13 +8,21 @@ const checkUrl = require('./routemiddleware')
 
 
 app.get('/',function(req, res){
-  res.send('This is Home Page')
+  // res.send('This is Home Page')
+
+  res.sendFile(__dirname + "/Home.html")
 })
 app.get('/about', function(req,res){
-  res.send('This is About Page')
+  // res.send('This is About Page')
+
+    res.sendFile(__dirname + "/About.html")
+
 })
 router.get('/login', checkUrl, function(req,res){
-  res.send('This is Login Page')
+  // res.send('This is Login Page')
+
+    res.sendFile(__dirname + "/Login.html")
+
 })
 app.use('/',router)
 app.listen(4000)
