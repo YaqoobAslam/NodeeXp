655a2cff6fd54e00a6e75a947d252f92bc1fd900
Yaqoob <happyyaqoob@gmail.com>
2021-02-22 02:28:45 -0800

    Dynamic Content in Node

--- first-app/app.js
+++ first-app/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
 app.get("/profile/:name", function (req, res) {
-  console.warn(req.params.name);
-  res.render("Profile", { name: req.params.name });
+
+  // console.warn(req.params.name)
+  data = {email: 'test@gmail.com',address: 'noida', skills:['node js', 'javascript', 'vue']}
+  res.render("Profile", { name: req.params.name, data: data})
 });
 
 app.listen(4000);
