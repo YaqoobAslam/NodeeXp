d0b2547d0e03c19352755dcb95ca38e4ab0574e6
Yaqoob <happyyaqoob@gmail.com>
2021-02-22 03:51:26 -0800

    Add static file | add css Make routed middleware and use express static built-in middleware

--- first-app/app.js
+++ first-app/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+app.use('/assets',express.static('assets'))
+
 app.set("view engine", "ejs");
 app.get("/profile/:name", function (req, res) {
 
