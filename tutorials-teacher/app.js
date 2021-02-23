2d5db8fd4185bb2629785d18c29ad025cb420fa4
Yaqoob <happyyaqoob@gmail.com>
2021-02-23 13:10:33 -0800

    Export Module in Node.js

---
+++ tutorials-teacher/app.js
@@ -0,0 +1,10 @@
+var myLogModule = require('./Log')
+
+myLogModule.info('Node.js started')
+
+var msg = require('./Message')
+console.log(msg);
+
+var msg = require('./Message')
+console.log(msg.SimpleMessage);
+
