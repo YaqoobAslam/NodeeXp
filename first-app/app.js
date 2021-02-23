5701e51b4d52dd3837ed79997eba870834a3d83b
Yaqoob <happyyaqoob@gmail.com>
2021-02-23 06:13:46 -0800

    Put API | mongodb | express js

--- first-app/app.js
+++ first-app/app.js
@@ -4,9 +4,8 @@ const mongoose = require("mongoose");
 
 const User = require("./models/users");
 
-var bodyParser = require('body-parser')
+var bodyParser = require("body-parser");
 var jsonParser = bodyParser.json();
-
 
 mongoose.connect(
   "mongodb+srv://YaqoobAslam:Ahmad786@786$yokihahalols@cluster0.lx1pt.mongodb.net/Test?retryWrites=true&w=majority",
@@ -17,35 +16,50 @@ mongoose.connect(
 );
 
 app.get("/users", function (req, res) {
-  User.find().select('name').then((data) => {
-    res.status(201).json(data);
-  });
+  User.find()
+    .select("name")
+    .then((data) => {
+      res.status(201).json(data);
+    });
 });
 
-app.post("/users", jsonParser, function(req, res){
+app.post("/users", jsonParser, function (req, res) {
   // res.end('Post Api will be here.')
 
   const data = new User({
-    _id: new mongoose.Types.ObjectId,
+    _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     email: req.body.email,
-    address: req.body.address
-  })
+    address: req.body.address,
+  });
 
-  data.save().then((result)=>{
-    res.status(200).json(result)
-  })
-  .catch((errer)=>{
-    console.warn(errer);
-  })
-})
+  data
+    .save()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((errer) => {
+      console.warn(errer);
+    });
+});
 
-app.delete("/user/:id", function(req,res){
-  User.deleteOne({_id:req.params.id}).then((result)=>{
-    res.status(200).json(result)
-  }).catch((errer)=>{
-    console.warn(errer);
-  })
-})
+app.delete("/user/:id", function (req, res) {
+  User.deleteOne({ _id: req.params.id })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((errer) => {
+      console.warn(errer);
+    });
+});
+
+app.put("/user/:id", jsonParser, function (req, res) {
+  User.updateOne(
+    { _id: req.params.id },
+    { $set: { name: req.body.name } }
+  ).then((result) => {
+    res.status(200).json(result);
+  }) .catch((errer) => {console.warn(errer)});
+});
 
 app.listen(5000);
