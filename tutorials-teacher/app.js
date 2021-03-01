b6f9117e80e9357492c42add04f0770c4e2934ec
Yaqoob <happyyaqoob@gmail.com>
2021-03-01 10:14:24 -0800

    commit

--- tutorials-teacher/app.js
+++ tutorials-teacher/app.js
@@ -1,20 +1,19 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/submit-student-data', function (req, res) {
-    var name = req.body.firstName + ' ' + req.body.lastName;
-    
-    res.send(name + '  Submitted Successfully!');
+app.post("/submit-student-data", function (req, res) {
+  var name = req.body.firstName + " " + req.body.lastName;
+
+  res.send(name + "  Submitted Successfully!");
 });
 
 app.listen(4000, function () {
-    console.log('Node server is running..');
+  console.log("Node server is running..");
 });
-
