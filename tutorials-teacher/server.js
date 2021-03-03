3ec89c9869070299d72ee9f995bff04e99a14fdc
Yaqoob <happyyaqoob@gmail.com>
2021-03-03 00:58:01 -0800

    GruntJS

--- tutorials-teacher/server.js
+++ tutorials-teacher/server.js
@@ -36,16 +36,16 @@
 // });
 
 
-var express = require('express')
-var app = express()
+// var express = require('express')
+// var app = express()
 
-app.set("view engine", "vash")
-app.get('/', function(req, res){
-  res.render('index', { title: 'Vash Template Demo', 
-                          content:'This is dummy paragraph.'});
+// app.set("view engine", "vash")
+// app.get('/', function(req, res){
+//   res.render('index', { title: 'Vash Template Demo', 
+//                           content:'This is dummy paragraph.'});
 
-})
-  var server = app.listen(5000, function(){
-    console.log('Node server is running..');
-  })
+// })
+//   var server = app.listen(5000, function(){
+//     console.log('Node server is running..');
+//   })
 
