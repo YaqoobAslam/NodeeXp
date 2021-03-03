6e893dc77ae5277aaa5cd7168bf6ccca16a40db0
Yaqoob <happyyaqoob@gmail.com>
2021-03-02 22:00:01 -0800

    Fetching data from the database

--- tutorials-teacher/app.js
+++ tutorials-teacher/app.js
@@ -34,3 +34,14 @@ MongoClient.connect("mongodb://localhost:27017/MyDb", function (err, db) {
     });
   });
 });
+
+var MongoClient = require("mongodb").MongoClient;
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/MyDb", function (err, db) {
+  db.collection("Persons", function (err, collection) {
+    collection.find().toArray(function (err, items) {
+      if (err) throw err;
+      console.log(items);
+    });
+  });
+});
