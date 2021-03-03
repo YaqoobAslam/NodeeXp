736a66f8dd3ba70a201a72c23cdcedebde5e3aaf
Yaqoob <happyyaqoob@gmail.com>
2021-03-03 05:48:10 -0800

    commit

--- javatpoint/dns_example1.js
+++ javatpoint/dns_example1.js
@@ -1,14 +1,5 @@
-const dns = require('dns');
-const { hostname } = require('os');
-dns.resolve4('www.javatpoint.com', (err, addresses) => {
-  if(err) throw err;
-  console.log(`addresses: ${JSON.stringify(addresses)}`);
-  addresses.forEach((a) => {
-    dns.reverse(a, (err, hostnames) => {
-      if(err) {
-        throw err
-      }
-      console.log(`reverse for ${a}: ${JSON.stringify(hostnames)}`);
-    })
-  })
+const dns = require('dns')
+const { hostname } = require('os')
+dns.lookupService('127.0.0.1', 22,(err, hostname, service) => {
+  console.log(hostname, service);
 })
