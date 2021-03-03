9515056f39da0d092cffd95234aaeabbb48d9810
Yaqoob <happyyaqoob@gmail.com>
2021-03-03 00:16:14 -0800

    Vash Template Engine

--- tutorials-teacher/server.js
+++ tutorials-teacher/server.js
@@ -1,36 +1,51 @@
-var express = require("express");
-var app = express();
+// var express = require("express");
+// var app = express();
 
 // set view engine
-app.set("view engine", "jade");
-app.get("/", function (req, res) {
-  res.render("sample");
-});
+// app.set("view engine", "jade");
+// app.get("/", function (req, res) {
+//   res.render("sample");
+// });
 
-var server = app.listen(5000, function () {
-  console.log("Node server is running..");
-});
+// var server = app.listen(5000, function () {
+//   console.log("Node server is running..");
+// });
 
 
 
-var express = require("express");
-var app = express();
+// var express = require("express");
+// var app = express();
 
-app.set("view engine", "jade");
-app.get("/", function (req, res) {
-  var sql = require("mssql");
-  var config = {
-    user: "sa",
-    password: "sjkaria",
-    server: "localhost",
-    database: "SchoolDB",
-  };
-  sql.connect(config, function (err) {
-    if (err) console.log(err);
-    var request = new sql.Request();
-    request.query("select * from Student", function (err, recordset) {
-      if (err) console.log(err);
-      else res.render("StudentList", { studentList: recordset });
-    });
-  });
-});
+// app.set("view engine", "jade");
+// app.get("/", function (req, res) {
+//   var sql = require("mssql");
+//   var config = {
+//     user: "sa",
+//     password: "sjkaria",
+//     server: "localhost",
+//     database: "SchoolDB",
+//   };
+//   sql.connect(config, function (err) {
+//     if (err) console.log(err);
+//     var request = new sql.Request();
+//     request.query("select * from Student", function (err, recordset) {
+//       if (err) console.log(err);
+//       else res.render("StudentList", { studentList: recordset });
+//     });
+//   });
+// });
+
+
+var express = require('express')
+var app = express()
+
+app.set("view engine", "vash")
+app.get('/', function(req, res){
+  res.render('index', { title: 'Vash Template Demo', 
+                          content:'This is dummy paragraph.'});
+
+})
+  var server = app.listen(5000, function(){
+    console.log('Node server is running..');
+  })
+
