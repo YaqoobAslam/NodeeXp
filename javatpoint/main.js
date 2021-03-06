b9cf594b6baa6c2cdef8d85b9d76b1e9a52aa1b7
Yaqoob <happyyaqoob@gmail.com>
2021-03-06 01:55:53 -0800

    Node.js Open a file

--- javatpoint/main.js
+++ javatpoint/main.js
@@ -1,15 +1,9 @@
 var fs = require('fs')
-var zlib = require('zlib')
-// Compress the file input.txt to input.txt.gz  
-fs.createReadStream('input.txt')
-.pipe(zlib.createGzip())
-.pipe(fs.createWriteStream('input.txt.gz'))
-console.log('File Compressed.')
-
-var fs = require("fs");  
-var zlib = require('zlib');  
-// Decompress the file input.txt.gz to input.txt  
-fs.createReadStream('input.txt.gz')  
-  .pipe(zlib.createGunzip())  
-  .pipe(fs.createWriteStream('input.txt'));  
-  console.log("File Decompressed."); 
+//  Asynchronous - Opening File
+console.log('Going to open file!')
+fs.open('input.txt', 'r+', function(err, fd){
+  if(err){
+    return console.error(err)
+  }
+console.log("File opened successfully!")
+})
